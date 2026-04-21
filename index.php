d9e3be1f0b73804ab6510f5a6dbe825eca8bbe12
Mary Byrne <maryebyrne2@gmail.com>
2026-04-21 14:56:33 +0100

    first commit, header complete, grid.css complete

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/grid.css">
+
+    <title>Student Accommodation Manager</title>
+
+</head>
+
+<body>
+
+<!-- start of header -->
+ <header>
+
+     <div class="container">
+        <!-- logo & nav bar -->
+        <div class="width-4"><i class="fa-sharp-duotone fa-light fa-people-roof"></i></div>
+        <div class="width-8">
+            <nav>
+                <ul>
+                    <li><a href=""><i class="fa-solid fa-angle-down"></i>About Us</a></li>
+                    <li><a href=""><i class="fa-solid fa-arrow-right-to-bracket"></i>Login or Signup</a></li>
+                    <li><a href=""><i class="fa-solid fa-circle-user"></i>Account</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <!-- search box & image -->
+        <div class="width-2">
+            <h1>Search, find and book your new home!</h1>
+
+            <div class="searchBox">
+                <h5>Search by city, university or property</h5>
+                <form action="">
+                    <input type="text" value="Search"><button class="searchButton">Search</button>
+                </form>
+                <p><a href="">Log in</a> to view recent properties</p>
+            </div>
+        </div>
+
+     </div> 
+    </header>
+    <!-- end of header -->
+    
+</body>
+
+</html>
+
+//http://localhost/studentAccomManager/index.php
